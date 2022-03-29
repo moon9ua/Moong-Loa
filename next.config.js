c179a1ec5b7c2814aef0c775cbe80405b5d338be
moon9ua <koom6@naver.com>
2022-03-29 09:05:33 +0900

    chore: github pages 배포 세팅

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const debug = process.env.NODE_ENV !== "production";
+const name = "Moong-Loa";
+
+module.exports = {
+  ...nextConfig,
+  assetPrefix: !debug ? `/${name}` : "",
+};
